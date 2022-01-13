d42433699d2e54a1fbe312210575db403be04030
Deneno <Deneno@yandex.ru>
2022-01-13 22:08:22 +0300

    Изучаем Retrofit

--- app/src/main/java/ya/deneno/retrofitapp/screens/splash/SplashFragment.kt
+++ app/src/main/java/ya/deneno/retrofitapp/screens/splash/SplashFragment.kt
@@ -5,18 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.coroutines.*
 import ya.deneno.retrofitapp.R
+import ya.deneno.retrofitapp.databinding.FragmentCashBinding
+import ya.deneno.retrofitapp.databinding.FragmentSplashBinding
 
 
 class SplashFragment : Fragment() {
+    private lateinit var binding: FragmentSplashBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_splash, container, false)
+        binding = FragmentSplashBinding.inflate(layoutInflater, container, false)
+        CoroutineScope(Dispatchers.Main).launch {
+            delay(1500)
+            Navigation.findNavController(binding.root).navigate(R.id.action_splashFragment_to_rootFragment)
+        }
+        return binding.root
     }
 
 }
